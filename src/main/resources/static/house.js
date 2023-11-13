2cc296d538d44ff72bf19c8de2e8519e64d4b564
Maddynenn <maddynenninger@gmail.com>
2023-11-13 07:39:04 -0800

    created canvas

--- src/main/resources/static/house.js
+++ src/main/resources/static/house.js
@@ -1,25 +1,29 @@
 var config = {
-        type: Phaser.AUTO,
-        width: 1000,
-        height: 800,
-
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: {y: 30},
-                debug: false
-            }
-        },
-        scene: {
-            preload: preload,
-            create: create,
-            update: update
-        }
-    };
-
-    var game = new Phaser.Game(config);
-
-    fucntion preload(){
-        
-
+    scale: {
+            mode: Phaser.Scale.FIT,
+             // ...
+            },
+    //width: 1200,
+    //height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+    this.scale.displaySize.setAspectRatio( 16/8.8);
+    this.scale.refresh();
+}
+
+function update ()
+{
+}
