e35e02893765c46f4d11b14929e05f5a41ab8922
fmullen24 <112408474+fmullen24@users.noreply.github.com>
2023-11-13 08:53:15 -0800

    commiting index.html

--- src/main/resources/static/Introduction.js
+++ src/main/resources/static/Introduction.js
@@ -1,4 +1,4 @@
-    var config = {
+    /*var config = {
         type: Phaser.AUTO,
         width: 1920,
         height: 1080,
@@ -20,5 +20,61 @@
 var game = new Phaser.Game(config);
 function preload(){
 
-}
+}*/
 //trying to set up intro and canvas did not work
+
+//maddy's version: now this just makes a white screen
+/*var config = {
+    scale: {
+            mode: Phaser.Scale.FIT,
+             // ...
+            },
+    //width: 1200,
+    //height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+    this.scale.displaySize.setAspectRatio( 16/8.8);
+    this.scale.refresh();
+}
+
+function update ()
+{
+}*/
+//this was copied from the PhaserTutorial code
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
