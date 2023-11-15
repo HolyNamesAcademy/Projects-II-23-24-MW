967dcc0e9d1407d8b011e7bcc69ae2efaad4109e
josiePottier <jpottier24@holynames-sea.org>
2023-11-15 08:52:02 -0800

    made an image appear on the screen *^_^*

--- src/main/resources/static/house.js
+++ src/main/resources/static/house.js
@@ -1,10 +1,7 @@
 var config = {
-    scale: {
-            mode: Phaser.Scale.FIT,
-             // ...
-            },
-    //width: 1200,
-    //height: 600,
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
     scene: {
         preload: preload,
         create: create,
@@ -16,12 +13,14 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('sky', 'sky.png');
 }
 
 function create ()
 {
     this.scale.displaySize.setAspectRatio( 16/8.8);
     this.scale.refresh();
+    this.add.image(400, 300, 'sky');
 }
 
 function update ()
