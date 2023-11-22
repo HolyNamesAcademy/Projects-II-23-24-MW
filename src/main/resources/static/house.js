f1542a5dd511c1fbf1883f1ade0aacfab0448e92
josiePottier <jpottier24@holynames-sea.org>
2023-11-22 07:48:10 -0800

    added platforms that we can reposition/change size and shape to become objects that we cant walk through ! :3

--- src/main/resources/static/house.js
+++ src/main/resources/static/house.js
@@ -1,4 +1,4 @@
-var config = {
+/*var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
@@ -21,6 +21,56 @@ function create ()
     this.scale.displaySize.setAspectRatio( 16/8.8);
     this.scale.refresh();
     this.add.image(400, 300, 'sky');
+}
+
+function update ()
+{
+}*/
+var config = {
+    scale: {
+        mode: Phaser.Scale.FIT,
+    },
+    //width: 800,
+    //height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+       },
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'sky.png');
+    this.load.image('ground', 'platform.png')
+}
+
+var platforms;
+function create ()
+{
+    this.scale.displaySize.setAspectRatio( 16/8.8);
+    this.scale.refresh();
+    this.add.image(400, 300, 'sky');
+
+
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
