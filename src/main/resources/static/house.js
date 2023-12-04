b40e98472ca88de9678ffbe251c26a9156305104
anaiysaa <anaiysaadeswal@outlook.com>
2023-12-04 08:51:15 -0800

    character walks!

--- src/main/resources/static/house.js
+++ src/main/resources/static/house.js
@@ -22,6 +22,7 @@ function preload ()
 {
     this.load.image('sky', 'sky.png');
     this.load.image('ground', 'platform.png')
+    this.load.spritesheet('dude', 'dude.png',{ frameWidth: 32, frameHeight: 48 });
 }
 
 var platforms;
@@ -38,8 +39,49 @@ function create ()
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    //player
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
-function update ()
-{
+function update (){
+    cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown){
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown){
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else{
+    player.setVelocityX(0);
+    player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown){
+        player.setVelocityY(-330);
+    }
 }
