68392f37490df130f5d88164240c1bafbd3d606a
fmullen24 <112408474+fmullen24@users.noreply.github.com>
2023-12-13 07:53:44 -0800

    Changing the background to be green and have the introduction text on it

--- src/main/resources/static/Introduction.js
+++ src/main/resources/static/Introduction.js
@@ -1,3 +1,11 @@
+
+class Introduction extends Phaser.Scene{
+
+constructor ()
+    {
+        super({ key: 'introduction' });
+    }
+
 var config = {
     scale: {
         mode: Phaser.Scale.FIT,
@@ -28,10 +36,87 @@ function create ()
    var score = 0;
    var scoreText;
    scoreText = this.add.text(10, 16, '\n  Welcome to a world where dogs reign...\n               Doglandia \n     But as you explore this cabin \n         in the woods, be alert, \n  for something is amiss in the forest.', { fontSize: '32px', fill: '#000' });
-
 }
 
 function update ()
 {
     cursors = this.input.keyboard.createCursorKeys();
 }
+
+ create ()
+
+ this.input.once('pointerdown', function ()
+        {
+  this.add.sprite(400, 300, 'face').setAlpha(0.2);
+
+            console.log('From SceneA to SceneB');
+
+            this.scene.start('House');
+
+        }, this);
+}
+
+class Sky extends Phaser.scene{
+
+    create ()
+    {
+
+
+    constructor ()
+        {
+            super({ key: 'sky' });
+        }
+
+    var config = {
+        scale: {
+            mode: Phaser.Scale.FIT,
+        },
+        width: 800,
+        height: 400,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);
+
+    function preload ()
+    {
+        this.load.image('example', 'sky.png');
+    }
+
+    function create ()
+    {
+
+       this.scale.displaySize.setAspectRatio( 2/1);
+       this.scale.refresh();
+       var bg= this.add.image(400, 200, 'example');
+        bg.setDisplaySize(800,400);
+
+
+
+        this.arrow = this.add.sprite(400, 300, 'arrow').setOrigin(0, 0.5);
+
+        this.input.once('pointerdown', function (event)
+        {
+
+            console.log('From Introduction to SkyScene');
+
+            this.scene.start('sky');
+
+        }, this);
+    }
+}
+}
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 400,
+    backgroundColor: '#000000',
+    parent: 'phaser-example',
+    scene: [ Introduction, Sky]
+};
+
+const game = new Phaser.Game(config);
